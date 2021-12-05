3d0395efa41794a14a5855decff6044f58adfcd6
Hiren Vadher <hirenvadher954@gmail.com>
2021-12-05 23:43:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/profile/:id', require('./routes/api/profile'));
+
 
 const PORT = process.env.PORT || 5000;
 
